15efb460c2e92034bda5189cc9f4817d7b28e3f6
Steven Zhang <stevezzhang@gmail.com>
2023-09-11 11:05:44 -0400

    Activity 2 (1 point): Branching and merging

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# Create a Python program that, when run on the “develop” branch, will print ‘Hello World’
+# to the terminal, name this file “helloworld.py”. Commit the change.
+
+print("Hello World")
